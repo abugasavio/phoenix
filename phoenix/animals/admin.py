dc6a41f651d8fa2d41749e166e2bd86cba5b3483
savioabuga <savioabuga@gmail.com>
2015-10-24 19:20:16 +0300

    adding admin settings

--- phoenix/animals/admin.py
+++ phoenix/animals/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
-from .models import Animal
+from .models import Animal, Breed, Breeder, Color, Sire, Dam
 
 
 @admin.register(Animal)
 class AnimalAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Breeder)
+class BreederAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Sire)
+class SireAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Dam)
+class DamAdmin(admin.ModelAdmin):
     pass
